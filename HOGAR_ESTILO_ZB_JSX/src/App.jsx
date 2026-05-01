76ae736084100b145189f8ea1b7468e413f3aebd
Beha19 <tu-email-de-beha19@gmail.com>
2026-05-01 09:00:14 -0500

    feat: diseño Marquee en App

---
+++ HOGAR_ESTILO_ZB_JSX/src/App.jsx
@@ -0,0 +1,14 @@
+import Marquee from "./components/Marquee"
+
+
+import Estilos from "./styles/estilos.css"
+
+function App() {
+    return(
+        <div>
+            <Marquee/>
+        </div>    
+    )
+}
+
+export default App
