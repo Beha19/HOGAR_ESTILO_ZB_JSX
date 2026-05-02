8fce5182b416e833f43a1f9fc5904fa6ec048479
Beha19 <tu-email-de-beha19@gmail.com>
2026-05-01 22:23:00 -0500

    feat: creacion de Navbar

--- HOGAR_ESTILO_ZB_JSX/src/components/Navbar.jsx
+++ HOGAR_ESTILO_ZB_JSX/src/components/Navbar.jsx
@@ -1,28 +1,33 @@
-import { Link } from "react-router-dom"
 import Wsp from "../assets/Wsp 3.png"
 import Fb from "../assets/Fb 1.png"
-import TikTok from "../assets/Tik Tok1.png"
+import TikTok from "../assets/Tik Tok 1.png"
 import Insta from "../assets/Insta 2.png"
 
 function Navbar() {
     return(
         <section id="Barra_Navegador">
-            <div className="Navegador">
-                <nav>
-                    <Link to="">Inicio</Link>
-                    <Link to="">Nosotros</Link>
-                    <Link to="">Tienda</Link>
-                    <Link to="">Contacto</Link>
-                    <Link to="">Formas de Pago</Link>
-                </nav>
-            </div>
-            <div className="Redes_Header">
-                <nav>
-                    <img src={Wsp} alt="WhastApp"></img>
-                    <img src={Fb} alt="Facebook"></img>
-                    <img src={TikTok} alt="Tik Tok"></img>
-                    <img src={Inta} alt="Instagram"></img>
-                </nav>
+            <nav>
+                <ul className="Navegador">         
+                    <li><a href="">Inicio</a></li>
+                    <li><a href="">Nosotros</a></li>
+                    <li><a href="">Tienda</a></li>
+                    <li><a href="">Contacto</a></li>
+                    <li><a href="">Formas de Pago</a></li>
+                </ul>
+            </nav>
+            <div className="Redes1">
+                <a href="" target="_blank">
+                    <img src={Wsp} alt="WhatsApp" />
+                </a>
+                <a href="" target="_blank">
+                    <img src={Fb} alt="Facebook" />
+                 </a>
+                <a href="" target="_blank">
+                    <img src={TikTok} alt="TikTok" />
+                </a>
+                <a href="" target="_blank">
+                    <img src={Insta} alt="Instagram" />
+                </a>
             </div>
         </section>
     )
