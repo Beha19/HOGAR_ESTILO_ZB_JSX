b25144755fc5c1916bee021e6d6ce7602d42b022
Beha19 <tu-email-de-beha19@gmail.com>
2026-05-01 22:23:28 -0500

    feat: agregar Navbar en App.jsx

--- HOGAR_ESTILO_ZB_JSX/src/App.jsx
+++ HOGAR_ESTILO_ZB_JSX/src/App.jsx
@@ -1,5 +1,6 @@
 import Marquee from "./components/Marquee"
 import HeaderBuscador from "./components/HeaderBuscador"
+import Navbar from "./components/Navbar"
 
 import "./styles/Estilos.css";
 
@@ -8,6 +9,7 @@ function App() {
         <div>
             <Marquee/>
             <HeaderBuscador/>
+            <Navbar/>
         </div>    
     );
 }
