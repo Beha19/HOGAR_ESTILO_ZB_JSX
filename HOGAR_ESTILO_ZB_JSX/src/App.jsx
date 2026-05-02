3f0c8c5dd039fd899b9fe3a8bbcf2affd874f56a
Beha19 <tu-email-de-beha19@gmail.com>
2026-05-02 16:05:27 -0500

    feat: actulizacion de App.jsx

--- HOGAR_ESTILO_ZB_JSX/src/App.jsx
+++ HOGAR_ESTILO_ZB_JSX/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Inicio from "./pages/Inicio"
 import Nosotros from "./pages/Nosotros"
+import Tienda from "./pages/Tienda"
+import Contacto from "./pages/Contacto"
 import FormasPago from "./pages/FormasPago"
 
 import "./styles/estilos.css"
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Inicio />} />
         <Route path="/nosotros" element={<Nosotros />} />
         <Route path="/formas-pago" element={<FormasPago />} />
+        <Route path="/tienda" element={<Tienda />} />
+        <Route path="/contacto" element={<Contacto />} />
       </Routes>
     </BrowserRouter>
   )
