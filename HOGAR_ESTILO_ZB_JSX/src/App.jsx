7ec290821906d5d931f1a1c64e3a6ce7131bec9d
Beha19 <tu-email-de-beha19@gmail.com>
2026-05-02 07:11:05 -0500

    feat: agregar Tiempo a App.jsx

--- HOGAR_ESTILO_ZB_JSX/src/App.jsx
+++ HOGAR_ESTILO_ZB_JSX/src/App.jsx
@@ -2,9 +2,13 @@ import Marquee from "./components/Marquee"
 import HeaderBuscador from "./components/HeaderBuscador"
 import Navbar from "./components/Navbar"
 import Banner from "./components/Banner"
+import Tiempo from "./components/Tiempo"
 import Ventajas from "./components/Ventajas"
 import Ofertas from "./components/Ofertas"
 import ReelFb from "./components/ReelsFb"
+import Comentarios from "./components/Comentarios"
+import Beneficios from "./components/Beneficios"
+import Footer from "./components/Footer"
 
 import "./styles/estilos.css";
 
@@ -15,9 +19,13 @@ function App() {
             <HeaderBuscador/>
             <Navbar/>
             <Banner/>
+            <Tiempo/>
             <Ventajas/>
             <Ofertas/>
             <ReelFb/>
+            <Comentarios/>
+            <Beneficios/>
+            <Footer/>
         </div>    
     );
 }
